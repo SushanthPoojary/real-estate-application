a683709280008d3d65322861adb5b0095f34ca58
Sushanth Poojary <Sushanth.Poojary@teknopoint.local>
2024-07-17 04:12:04 +0530

    api config and setup

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+    console.log("Register Route");
+}
+
+export const login = (req, res) => {
+    console.log("login Route");
+}
+
+export const logout = (req, res) => {
+    console.log("logout Route");
+}
