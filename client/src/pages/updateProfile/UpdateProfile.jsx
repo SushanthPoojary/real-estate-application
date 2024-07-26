ef4f2f6babb478a3e17d598d8e5dd448ca742fdf
Sushanth Poojary <Sushanth.Poojary@teknopoint.local>
2024-07-26 18:46:36 +0530

    small screen ui changes

--- client/src/pages/updateProfile/UpdateProfile.jsx
+++ client/src/pages/updateProfile/UpdateProfile.jsx
@@ -11,6 +11,7 @@ function UpdateProfile() {
             <div className="form-cont">
                 <form>
                     <h1>Update Profile</h1>
+                    <img className="avatar-small" src={currentUser.avatar || "/avatar.png"} alt="" />
                     <input name="username" type="text" placeholder="Username" defaultValue={currentUser.username} />
                     <input name="email" type="text" placeholder="Email" defaultValue={currentUser.email} />
                     <input name="password" type="password" placeholder="Password" />
