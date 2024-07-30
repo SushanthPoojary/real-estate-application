0a2eaa98bcd4d7c320fda998ab69e35050b6362c
Sushanth Poojary <Sushanth.Poojary@teknopoint.local>
2024-07-30 18:37:00 +0530

    posts api routes implementation

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import usersRoute from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use(cookieParser());
 app.use("/api/users", usersRoute);
+app.use("/api/posts", postRoutes);
 
 app.listen(4507, () => {
     console.log("Server is running");
